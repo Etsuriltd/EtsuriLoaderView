fec9bbafe378a884f8af12db161b659dfeaf4c7d
wahyu sumartha <wahyusumartha@gmail.com>
2016-08-25 18:05:26 +0700

    ^fix loader so that it could load more than one at the same time

--- EtsuriLoaderView/Classes/EtsuriLoaderView.swift
+++ EtsuriLoaderView/Classes/EtsuriLoaderView.swift
@@ -9,38 +9,46 @@
 import UIKit
 
 public class EtsuriLoaderView: UIView {
-
-    public static let instance = EtsuriLoaderView()
     
     private let logoLayer = LogoLayer()
     
-    public init() {
-        let screenWidth = UIScreen.mainScreen().bounds.size.width
-        let screenHeight = UIScreen.mainScreen().bounds.size.height
+    public init(screenAdjustment point: CGPoint) {
+        let screenWidth = UIScreen.mainScreen().bounds.size.width - point.x
+        let screenHeight = UIScreen.mainScreen().bounds.size.height - point.y
         
         let originX = (screenWidth - maxWidth) / 2
         let originY = (screenHeight - maxHeight) / 2
         
         super.init(frame: CGRectMake(originX, originY, maxWidth, maxHeight))
         self.backgroundColor = UIColor.clearColor()
-       
+        
         self.layer.addSublayer(logoLayer)
     }
     
-    public class func showLoading(fromView view: UIView) {
-        let loadingView = EtsuriLoaderView.instance
+    public class func showLoading(fromView view: UIView, screenAdjustment point: CGPoint) {
+        let loadingView = EtsuriLoaderView(screenAdjustment: point)
         view.addSubview(loadingView)
     }
     
     public class func hideLoadingView(fromView view: UIView) {
-        let loadingView = EtsuriLoaderView.instance
-        view.willRemoveSubview(loadingView)
-        loadingView.removeFromSuperview()
+        let loadingView = self.loaderForView(view)
+        if let currentLoadingView = loadingView {
+            currentLoadingView.removeFromSuperview()
+        }
+    }
+    
+    public class func loaderForView(view: UIView) -> EtsuriLoaderView? {
+        let subviewsEnum = view.subviews.reverse()
+        for subview in subviewsEnum {
+            if subview is EtsuriLoaderView {
+                return (subview as? EtsuriLoaderView)
+            }
+        }
+        return nil
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
     
 }
